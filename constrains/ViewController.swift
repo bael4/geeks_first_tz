93122e18c64ea3f2467973ec653ff8cbb7e5646d
bael4 <bayel.ryspekov@gmail.com>
2023-03-22 21:37:29 +0600

    some fix

--- constrains/ViewController.swift
+++ constrains/ViewController.swift
@@ -9,22 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var loginButton: UIButton!
+ 
     @IBOutlet weak var nameTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBOutlet weak var button: UIButton!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.systemBlue.cgColor
-        button.layer.backgroundColor = UIColor.white.cgColor
-        button.layer.cornerRadius = 10
-        
-        loginButton.layer.cornerRadius = 10
+//        
+//        button.layer.borderWidth = 1
+//        button.layer.borderColor = UIColor.systemBlue.cgColor
+//        button.layer.backgroundColor = UIColor.white.cgColor
+//        button.layer.cornerRadius = 10
+//
+//        loginButton.layer.cornerRadius = 10
     }
     
 
@@ -34,31 +34,32 @@ class ViewController: UIViewController {
         
 
 
-    
-    @IBAction func loginButton(_ sender: Any) {
+    @IBAction func logginAction(_ sender: Any) {
         
-                let nameCount = nameTextField.text?.count
-                let passwordCount =  passwordTextField.text?.count
         
+        let nameCount = nameTextField.text?.count
+        let passwordCount =  passwordTextField.text?.count
 
 
-                if nameCount! == 0 || nameCount! < 3 {
-                  nameTextField.layer.borderWidth = 2
-                    nameTextField.layer.borderColor = UIColor.red.cgColor
-                 nameTextField.placeholder =  "пользователь не найден"
-                }else{
-                   nameTextField.layer.borderColor = UIColor.clear.cgColor
-                }
 
-                if passwordCount! == 0 || passwordCount! < 8  {
-                    passwordTextField.layer.borderWidth = 2
-                    passwordTextField.layer.borderColor = UIColor.red.cgColor
-                    passwordTextField.placeholder = "слишком короткий пароль"
-                }else{
-                    passwordTextField.layer.borderColor = UIColor.clear.cgColor
-                }
-    
+        if nameCount! == 0 || nameCount! < 3 {
+          nameTextField.layer.borderWidth = 2
+            nameTextField.layer.borderColor = UIColor.red.cgColor
+         nameTextField.placeholder =  "пользователь не найден"
+        }else{
+           nameTextField.layer.borderColor = UIColor.clear.cgColor
+        }
+
+        if passwordCount! == 0 || passwordCount! < 8 {
+            passwordTextField.layer.borderWidth = 2
+            passwordTextField.layer.borderColor = UIColor.red.cgColor
+            passwordTextField.placeholder = "слишком короткий пароль"
+        }else{
+            passwordTextField.layer.borderColor = UIColor.clear.cgColor
+        }
         
     }
+    
+    
     
 }
